4721f16e4af7e430c1d285b2233f2f12c5155bd1
LuisMerc4do <163725779+LuisMerc4do@users.noreply.github.com>
2024-06-21 23:13:50 +1000

    Structure Creation

---
+++ src/components/InputField.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const InputField = () => {
+  return (
+    <form className="input">
+      <input
+        type="input"
+        placeholder="Enter a Task"
+        className="input__box"
+      ></input>
+      <button className="input__submit" type="submit">
+        Go
+      </button>
+    </form>
+  );
+};
+
+export default InputField;
